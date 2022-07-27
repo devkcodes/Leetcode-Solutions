2f45a756b801c78efe8a3259feb0fb1b0cd8e9e0
Devansh Katheria <68653927+devkcodes@users.noreply.github.com>
2022-07-27 15:44:20 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (44.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,20 +3,16 @@ public:
     int uniquePaths(int m, int n) {
         
         vector<vector<int>>cache(m+1,vector<int>(n+1));
-        cache[m-1][n-1]=1;
         
-       
+        cache[m-1][n-1]=1;
+        //start from bottom right. No. of ways to reach last cell is initially  1. 
+        
         for(int i=m-1;i>=0;i--)
         {
             for(int j=n-1;j>=0;j--)
-                cache[i][j]+=cache[i][j+1]+cache[i+1][j];
+                cache[i][j]+=cache[i][j+1]+cache[i+1][j]; //number of ways to reach each cell is (number of ways to reach cell to its right) + (no. of ways to reach cell below it)
         }
-        
-        
-        for(int i=0;i<m+1;i++)
-           { for(int j=0;j<n+1;j++)
-                cout<<cache[i][j]<<" ";
-            cout<<endl;}
+     
         
         return cache[0][0];
     }
