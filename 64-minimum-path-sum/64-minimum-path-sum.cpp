380095dd4b92cc5bdad6953964d7304e8bf49b62
Devansh Katheria <68653927+devkcodes@users.noreply.github.com>
2022-07-27 20:31:15 +0530

    Time: 16 ms (46.22%), Space: 10.1 MB (28.77%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -12,18 +12,18 @@ public:
         
         
         for(int i=1;i<m;i++)
-            dp[0][i]+=dp[0][i-1];
+            grid[0][i]+=grid[0][i-1];
         
         for(int i=1;i<n;i++)
-            dp[i][0]+=dp[i-1][0];
+            grid[i][0]+=grid[i-1][0];
         
         for(int i=1;i<n;i++)
         {
             for(int j=1;j<m;j++)
             {
-                int top = dp[i-1][j];
-                int left = dp[i][j-1];
-                dp[i][j]+=min(top,left);
+                int top = grid[i-1][j];
+                int left = grid[i][j-1];
+                grid[i][j]+=min(top,left);
                 
             }
         }
@@ -36,7 +36,7 @@ public:
 //             }
 //             cout<<endl;
 //         }
-        return dp[n-1][m-1];
+        return grid[n-1][m-1];
         
         
     }
