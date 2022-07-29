d8234b1584608ed4f8a0c30c431e64ac26436377
Devansh Katheria <68653927+devkcodes@users.noreply.github.com>
2022-07-30 03:11:47 +0530

    Time: 3 ms (89.85%), Space: 8 MB (92.58%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -3,6 +3,7 @@ public:
     vector<string> findAndReplacePattern(vector<string>& words, string p) {
         
         vector<int>map(27,0);
+        vector<string>ans;
         int s = p.length();
         
         string a=""; int counter=0;
@@ -14,46 +15,31 @@ public:
                 map[i-'a']=counter;
                 a+=counter+48;
             }
-            
-             else 
-               {
-                   
-                   a+=map[i-'a']+48;
-               }
+            else 
+              a+=map[i-'a']+48;
         }
-        
-        
-        cout<<a<<endl;
-            
-        
-        vector<string>ans;
-        
-        
+     
         for(auto str : words)
         {
             string b="";int count = 0;
+            
             vector<int>map2(27,0);
 
            for(auto i : str)
-        {
-            if(map2[i-'a']==0)
             {
-                count++;
-                map2[i-'a']=count;
-                b+=count+48;
-            }
-               else 
-               {
+                if(map2[i-'a']==0)
+                {
+                    count++;
+                    map2[i-'a']=count;
+                    b+=count+48;
+                }
+                   else 
                    b+=map2[i-'a']+48;
-               }
-            
-            
-        }
+                   
+            }
             
             if(a==b)
                 ans.push_back(str);
-            
-            cout<<b<<endl;
             
         }
         
