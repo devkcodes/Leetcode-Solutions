6d449b63f92182336da6894077c2325c289e792a
Devansh Katheria <68653927+devkcodes@users.noreply.github.com>
2022-07-30 03:17:27 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (84.30%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string p) {
         
-        vector<int>map(27,0);
+        vector<int>map(27,0); //declaring map to store occurance of characters of p
         vector<string>ans;
         int s = p.length();
         
         string a=""; int counter=0;
-        for(auto i : p)
+        for(auto i : p)// for every char in p
         {
-            if(map[i-'a']==0)
+            if(map[i-'a']==0)//if it has occured first time
             {
-                counter++;
-                map[i-'a']=counter;
-                a+=counter+48;
+                counter++;//assign it a new number
+                map[i-'a']=counter;//update the number in the map
+                a+=counter+48;//add corresponding number to resulting string 'a'
+                //+48 converts number 1 to character '1' so that it can be added to the string
             }
             else 
-              a+=map[i-'a']+48;
+              a+=map[i-'a']+48;//if already present then add corresponding number to resulting string 'a'
         }
      
-        for(auto str : words)
+        for(auto str : words)//for every word in words, repeat the same procedure and check if resulting string 'b' is equal to 'a', if it is, then add the word to ans vector
         {
             string b="";int count = 0;
             
