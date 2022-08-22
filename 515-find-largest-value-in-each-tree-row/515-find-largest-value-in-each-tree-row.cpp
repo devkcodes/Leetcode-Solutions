52d5257db1a48ad2273d36549bb993ff980843e4
Devansh Katheria <68653927+devkcodes@users.noreply.github.com>
2022-08-23 00:09:34 +0530

    Time: 11 ms (94.67%), Space: 22.1 MB (87.30%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    vector<int>ans;
+    vector<int>ans;//ans vector, storing the highest value at ith level at index i
     
     void helper(TreeNode* root, int i)
     {
-        if(root==NULL)
+        if(root==NULL)//base case
             return;
-        
         int n = size(ans)-1;
-        if(n<i)
-            ans.push_back(root->val);
+        if(n<i)//if no value for ith level is present,
+            ans.push_back(root->val); //add value to ith level
         else
-            ans[i]=max(ans[i],root->val);
+            ans[i]=max(ans[i],root->val);//else update the max of old and curr value
         
-        i++;
-        helper(root->left,i);
+        i++; //increase level count
+        helper(root->left,i); //process left and right subtree
         helper(root->right,i);
         
         
